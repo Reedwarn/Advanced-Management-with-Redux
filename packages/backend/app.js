b12ae0578778ce652223d59098b34bbfb5d7d323
Ridwan Oladapo <ridwankolawole93@gmail.com>
2024-10-02 07:20:15 +0000

    ready for deployment

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -18,6 +18,7 @@ app.use(cookieParser(config.TOKEN));
 const corsOptions = {
 	// Add your address here i.e. your forwarded address from a cloud environment
 	origin: [
+		"https://5174-reedwarn-arbitruml2-f0mlgz14mui.ws-eu116.gitpod.io",
 		"http://127.0.0.1:5173",
 		"http://127.0.0.1:4173",
 		"http://localhost:5173",
